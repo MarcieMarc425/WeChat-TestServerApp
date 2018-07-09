14e31d3fe8644bf22317611a174bfe4ca82e3982
marctse <marctse@tencent.com>
2018-07-09 11:03:20 +0800

    Updated server login verification

--- server/server.js
+++ server/server.js
@@ -2,81 +2,60 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
+var uuid = require('uuid/v4');
+var MongoClient = require('mongodb').MongoClient;
 var crypto = require('crypto');
 
 const port = 8081;
 
-app.get('*', function(req, res) {
-    // openid
-    // res.send({
-    //     openid: openid 
-    // })
-    // console.log('received request...');
-    // var appID = 'wxb5938a6e8e1cdfcf';
-    // var appSecret = '1c5d260657f55c8b47d4d810e6eb96dc';
-    // var code = req.code;
-    // console.log(req.data);
-});
+app.get('*', function (req, res) {});
+
+var user = {
+    openid: '',
+    session_key: '',
+    timeout: ''
+}
+
+var userHashMap = [];
 
 io.on('connect', socket => {
-    console.log('a user has connected');
-    socket.on('disconnect', () => {
-        console.log('a user has disconnected');
-    });
+
+    console.log('User has connected...');
+
     socket.on('login', res => {
+        console.log('User is attempting to login');
         var appID = 'wxb5938a6e8e1cdfcf';
         var appSecret = '1c5d260657f55c8b47d4d810e6eb96dc';
         var code = res;
         var url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appID + '&secret=' + appSecret + '&js_code=' + code + '&grant_type=authorization_code';
-        console.log(code);
         request(url, (error, response, body) => {
-            if(error)
+            if (error)
                 console.log(error)
             else {
-                var resPkg = JSON.parse(body);
-                console.log(resPkg.openid);
-                console.log(resPkg.session_key);
+                console.log('User has successfully logged in.');
+                var userObj = JSON.parse(body);
+                this.uuid = uuid();
+                userHashMap[this.uuid] = {
+                    openid: userObj.openid,
+                    session_key: userObj.session_key,
+                    timeout: userObj.expires_in
+                };
+                userObj = null;
+                socket.emit('login-success', this.uuid);
+                console.log(userHashMap[this.uuid]);
             }
-        })
+        });
+    });
+
+    socket.on('user-verify', () => {
+        
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected');
     });
 });
 
-// test.qq.com/login?code=dafafa
 http.listen(port, () => {
     console.log('listening on port ' + port);
 });
-
-// function getSessionKey (code, appid, appSecret) {
-//     var opt = {
-//         method: 'GET',
-//         url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
-//         params: {
-//             appid: appid,
-//             secret: appSecret,
-//             js_code: code,
-//             grant_type: 'authorization_code'
-//         }
-//     }
-
-//     return http(opt).then(function (res) {
-//         var data = res.data;
-//         if(!data.openid || !data.session_key || data.errcode) {
-//             return {
-//                 result: -2,
-//                 errmsg: data.errmsg || '返回数据字段不完整'
-//             }
-//         } else
-//             return data
-//     });
-// }
-
-// const crypto = require('crypto');
-
-// return getSessionKey(code, appid, secret).then(resData => {
-//     const {session_key} = resData;
-//     const eKey = encryptSha1(session_key);
-// });
-
-// function encryptSha1(data) {
-//     return crypto.createHash('sha1').update(data, 'utf8').digest('hex');
-// }
